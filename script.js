32fe0c5443f7d5a7825a9cc2811fe88d81873df1
Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>
2025-01-22 14:42:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
       { //id: "grid1", url: "https://elkem.sharepoint.com/sites/T.RAN-TappingogEtterbehandling/_layouts/15/embed.aspx?UniqueId=66f03f62-e9c0-4649-9dd6-117247089f5d#view=FitH"
         id: "grid1", url: "https://elkem.sharepoint.com/sites/T.RAN-TappingogEtterbehandling/_layouts/15/Doc.aspx?sourcedoc={11cd9663-4eac-4aec-b6ea-5c805e551377}&amp;action=embedview"
         },
-      { id: "grid2", url: "https://elkem.sharepoint.com/sites/T.RAN-TappingogEtterbehandling/_layouts/15/embed.aspx?UniqueId=6aad5651-48b0-4159-89b6-2ef42ea91144" },
+      { id: "grid2", url: "https://elkem.sharepoint.com/sites/T.RAN-TappingogEtterbehandling/_layouts/15/Doc.aspx?sourcedoc={d8f63b4c-e3ec-4d52-9927-9e35e0c7d3e2}&amp;action=embedview" },
       { id: "grid3", url: "img/prosess.PNG" },
       { id: "grid4", url: "https://planner.cloud.microsoft/webui/plan/5chRMHepAkaLt0GsUBkItZYAAQ_G/view/board?tid=80db3d04-4941-4992-9b01-cbe786893361" },
       { id: "grid5", url: "https://app.powerbi.com/reportEmbed?reportId=92780dd7-a4f8-4c35-a26a-a1255039429c&autoAuth=true&ctid=80db3d04-4941-4992-9b01-cbe786893361" },
